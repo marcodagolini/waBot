76735b4f28cb3c1a863c3882cdc540a939556151
marcodagolini <marco.dagolini@gmail.com>
2019-04-18 15:33:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,184 @@
-# waBot
+require('dotenv').config()
+
+
+
+
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+
+
+var https = require('https');
+var express = require('express');
+const Agent = require('node-agent-sdk').Agent;
+var echoAgent = new Agent({
+	accountId: '31554357',
+	username: 'waBot',
+	appKey: process.env.appKey,
+	secret: process.env.secret,
+	accessToken: process.env.accessToken,
+	accessTokenSecret: process.env.secretToken
+});
+
+
+echoAgent.on('connected', body =>{
+
+	console.log("");
+	console.log("");
+	console.log("");
+	console.log("*****connected")
+	console.log(JSON.stringify(body));
+	echoAgent.setAgentState({availability: "ONLINE"});
+	echoAgent.subscribeExConversations({
+		'agentIds': [echoAgent.agentId],
+		'convState': ['OPEN']
+	}, (e, resp) => console.log('subscribed successfully', echoAgent.conf.id || ''));
+	echoAgent.subscribeRoutingTasks({});
+
+});
+
+
+
+echoAgent.on('routing.RoutingTaskNotification', body =>{
+
+	if(!(body.changes.length < 1 || body.changes == undefined)){
+
+		body.changes.forEach(c => {
+			console.log("inside2");
+			if (c.type === "UPSERT") {
+				console.log("upsert");
+        echoAgent.getUserProfile(openConvs[c.result.conversationId].consumerID, (e, profileResp) => {
+          console.log(JSON.stringify(profileResp));
+        });
+				
+				c.result.ringsDetails.forEach(r => {
+					if (r.ringState === 'WAITING') {
+
+
+						echoAgent.updateRingState({
+							"ringId": r.ringId,
+							"ringState": "ACCEPTED"
+						}, (e, resp) => console.log(resp));
+
+                        			echoAgent.publishEvent({
+                            				"dialogId": c.result.dialogId,
+                            				"event": {
+                                				"type": "ChatStateEvent",
+                                				"chatState": "COMPOSING"
+                            				}
+                        			});
+						setTimeout(()=>{
+                        				echoAgent.publishEvent({
+                            					dialogId: c.result.dialogId,
+                            					event: {
+                                					type: 'ContentEvent',
+                                					contentType: 'text/plain',
+									message: 'Welcome message'
+                            					}
+                        				});
+						}, 3000);
+
+					}
+
+				});
+			}
+		});
+
+	}
+
+
+
+
+
+});
+
+
+
+
+echoAgent.on('ms.MessagingEventNotification', body =>{
+
+	console.log("");
+	console.log("");
+	console.log("");
+	console.log("*****ms.MessagingEventNotification")
+	console.log(JSON.stringify(body));
+
+});
+
+
+
+
+echoAgent.on('cqm.ExConversationChangeNotification', body =>{
+
+
+	if(!(body.changes.length < 1 || body.changes == undefined)){
+
+		console.log("inside1");
+
+		body.changes.forEach(c => {
+
+			if (c.type === "UPSERT") {
+				console.log("upsert");
+				
+				var myLength = c.result.conversationDetails.participants.length;
+				for (var i = 0; i < myLength; i++){
+					if(c.result.conversationDetails.participants[i].role === "CONSUMER"){
+						var myCustomer = c.result.conversationDetails.participants[i].id;
+					}
+				}
+        echoAgent.getUserProfile(myCustomer, (e, profileResp) => {
+          console.log(JSON.stringify(profileResp));
+        });
+
+			}
+		});
+
+	}
+
+
+
+
+});
+
+
+
+echoAgent.on('notification', body =>{
+
+	// triggered by all the notification events.
+
+});
+
+
+
+echoAgent.on('error', body =>{
+
+	console.log("");
+	console.log("");
+	console.log("");
+	console.log("*****error")
+	console.log(JSON.stringify(body));
+
+});
+
+
+
+
+echoAgent.on('closed', body =>{
+
+	console.log("");
+	console.log("");
+	console.log("");
+	console.log("*****closed")
+	console.log(JSON.stringify(body));
+
+});
+
+
+
+
+
+
+
+
+
+
