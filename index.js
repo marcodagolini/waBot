b97fe58c9a7c6bf31577354b450f03dcf87f4367
marcodagolini <marco.dagolini@gmail.com>
2019-04-21 12:54:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -131,6 +131,7 @@ function checkAuthentication(token, callback) {
 			'Authorization': oauth
     		}
 	}, function (e, r, b) {
+		console.log(JSON.stringify(b));
 		if (b){
 			callback (true);
 		} else{
