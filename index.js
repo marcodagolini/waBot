3c069667e79b8b1a943addefab93fc2eb1cf25c1
marcodagolini <marco.dagolini@gmail.com>
2019-10-06 13:19:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ var corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS. This is from --> ' + origin))
     }
   }
 }
