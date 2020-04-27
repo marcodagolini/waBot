3f17660234aa669bfcd2c04c3c354836d0dc95de
marcodagolini <marco.dagolini@gmail.com>
2020-04-27 09:58:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ function pushAgentData(agentId, concurrency, body, callback) {
 		
     	};
 	
-	body = delete body['passwordSh']
+	body.passwordSh = "";
 	body.maxAsyncChats = concurrency;
 	
 	var url = 'https://lo.ac.liveperson.net/api/account/31554357/configuration/le-users/users/' + agentId + '?v=4.0';
