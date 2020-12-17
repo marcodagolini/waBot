9cebfcddaabf112edfd2c279ac9b39a2118b87c0
marcodagolini <marco.dagolini@gmail.com>
2020-12-17 15:49:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -156,16 +156,7 @@ function uuidv4() {
 }
 
 
-function decodeBase64(s) {
-    var e={},i,b=0,c,x,l=0,a,r='',w=String.fromCharCode,L=s.length;
-    var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    for(i=0;i<64;i++){e[A.charAt(i)]=i;}
-    for(x=0;x<L;x++){
-        c=e[s.charAt(x)];b=(b<<6)+c;l+=6;
-        while(l>=8){((a=(b>>>(l-=8))&0xff)||(x<(L-2)))&&(r+=w(a));}
-    }
-    return r;
-};
+
 
 
 function checkFile(req, res, next) {
@@ -176,7 +167,7 @@ function checkFile(req, res, next) {
 	var binary = new Buffer(JSON.stringify(myPayload)).toString('binary')
 	// var binary = new Buffer(JSON.stringify(myPayload)).toString('base64')
 	
-	console.log(decodeBase64(binary));
+	console.log(btoa(unescape(encodeURIComponent(binary))));
 	
 	
 	
