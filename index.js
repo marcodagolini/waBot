10055b4a8c9f7a940cef82d2825c503bbc40a6f8
marcodagolini <marco.dagolini@gmail.com>
2020-12-18 22:56:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -181,17 +181,17 @@ function checkFile(req, res, next) {
 	// var tipeOfRequest = req.query.tipeOfRequest;
 	var myPayload = req.body;
 	
-	console.log(JSON.stringify(myPayload));
+	// console.log(JSON.stringify(myPayload));
 	// var binary = new Buffer(JSON.stringify(myPayload)).toString('binary');
 	
 	let objJsonStr = JSON.stringify(myPayload);
 	// let objJsonStr = JSON.stringify(myPayload,undefined,1);
-	// var binary = new Buffer(JSON.stringify(myPayload)).toString('binary');
+	var binary = new Buffer(JSON.stringify(myPayload)).toString('binary');
 	// objJsonStr = objJsonStr.substring(2, objJsonStr.length - 5);
 	// console.log(objJsonStr)
 	objJsonStr = Buffer.from(objJsonStr, 'binary').toString("base64");
 	// console.log("\n\n\n\n\n\n");
-	// console.log(objJsonB64)
+	console.log(objJsonStr)
 	// var btoa = require('btoa');
 	// console.log(objJsonStr);
 	
