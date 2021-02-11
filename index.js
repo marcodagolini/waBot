1fae71e11a41c4668a3e3798bb7f283e751f3692
marcodagolini <marco.dagolini@gmail.com>
2021-02-11 19:25:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -538,12 +538,22 @@ function testLogin(req, res, next) {
 	
 	console.log("decrypted --> " + decryptedString);
 	
+	var myTimestamp = 0;
+	
 	if(decryptedString.indexOf("timestamp-") > -1 && decryptedString.indexOf("-timestamp") > -1){
-		var myTimestamp = parseInt(decryptedString.replace("timestamp-","").replace("-timestamp",""));
+		myTimestamp = parseInt(decryptedString.replace("timestamp-","").replace("-timestamp",""));
 		console.log(myTimestamp);
 	}
 	
-	res.send("okGet");
+	var myResponse = "";
+	
+	if(myTimestamp > Date.now()){
+		myResponse = '<html><head><script></script><title>1685908</title></head><body>login success!!!</body></html>';
+	} else{
+		myResponse = '<html><head><script></script><title>1685908</title></head><body>session expired!!</body></html>';
+	}
+	
+	res.send(myResponse);
 	
 }
 
@@ -631,8 +641,8 @@ function testSaml(req, res, next) {
 			const encryptedTimestamp = cryptr.encrypt(expiring);
 			const decryptedString = cryptr.decrypt(encryptedTimestamp);
 			
-			console.log(encryptedTimestamp); // e7b75a472b65bc4a42e7b3f78833a4d00040beba796062bf7c13d9533b149e5ec3784813dc20348fdf248d28a2982df85b83d1109623bce45f08238f6ea9bd9bb5f406427b2a40f969802635b8907a0a57944f2c12f334bd081d5143a357c173a611e1b64a
-			console.log(decryptedString); // bacon
+			// console.log(encryptedTimestamp);
+			// console.log(decryptedString);
 			
 			myResponse = '<html><head><script type="text/javascript">document.cookie = "mySamlCookie=' + encryptedTimestamp + '";window.location.replace("https://marcowabot.herokuapp.com/testLogin");</script><title>1685908</title></head><body></body></html>'
 	
